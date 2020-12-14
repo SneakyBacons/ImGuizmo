532ff38645a9d2cfd7e2e058d7c79a54e3666dce
Scott Balfour <scott2004b@hotmail.com>
2020-12-14 07:28:13 +0000

    [~] Adding premake file and such

---
+++ premake5.lua
@@ -0,0 +1,34 @@
+project "ImGuizmo"
+	kind "StaticLib"
+	language "C++"
+
+    targetdir("bin/%{prj.name}/" .. outputdir )
+    objdir("bin-int/%{prj.name}/" .. outputdir )
+
+	files
+	{
+		"ImGuizmo.h",
+		"ImGuizmo.cpp",
+	}
+
+	includedirs
+	{
+		"../ImGui",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+        
+    filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Development"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
